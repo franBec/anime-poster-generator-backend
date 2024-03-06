3de8ae032335f5e429d6f845713a8973039410e8
Fran Bec <franbecvort@gmail.com>
2024-03-06 21:28:41 +0000

    initial commit

---
+++ src/main/java/dev/pollito/anime/poster/generator/backend/AnimePosterGeneratorBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.pollito.anime.poster.generator.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimePosterGeneratorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimePosterGeneratorBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/pollito/anime/poster/generator/backend/AnimePosterGeneratorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pollito.anime.poster.generator.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimePosterGeneratorBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
